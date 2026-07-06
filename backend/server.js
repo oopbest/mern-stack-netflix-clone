f58110f10476fe5447629e2d9457b033890061eb
Setthawut Pimthong <oop_best@yahoo.com>
2026-07-06 11:05:44 +0700

    feat: implement backend server with production build support and updated environment configurations

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
+import path from "path";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
@@ -11,6 +12,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json()); // for parsing application/json
 app.use(cookieParser());
@@ -19,6 +21,13 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
